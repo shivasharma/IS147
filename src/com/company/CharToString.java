099efc7ad90390a5aedb0225e96f10a439c570cd
Shiva Sharma <shivathebravo@gmail.com>
2015-08-30 23:10:09 -0400

    String

---
+++ src/com/company/CharToString.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Created by Shiva on 8/30/2015.
+ */
+public class CharToString {
+
+    public static void main(String[] args){
+        char[] chars= {'h','e','l', 'l','o'};
+        String s= new String(chars);
+        System.out.print(s);
+
+        String s1= "This is a string";
+        System.out.println(s1);
+
+
+        String s2= new String("This is a string");
+        System.out.println(s2);
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Enter Value");
+        String input= scanner.nextLine();
+        System.out.print(input);
+    }
+}
