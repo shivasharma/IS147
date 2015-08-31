c4f4f7e97ad46633aa918a5cd414cf3dbda72530
Shiva Sharma <shivathebravo@gmail.com>
2015-08-30 21:18:35 -0400

    Added print file

---
+++ src/Week2/Print/Java.java
@@ -0,0 +1,10 @@
+package Week2.Print;
+
+/**
+ * Created by Shiva on 8/30/2015.
+ */
+public class Java {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+}
