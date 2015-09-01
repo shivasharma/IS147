7b798f942477c896165ab5b5c605a3bbb030496f
Shiva Sharma <shivathebravo@gmail.com>
2015-08-31 22:47:10 -0400

    Added message

--- src/com/company/CharToString.java
+++ src/com/company/CharToString.java
@@ -9,21 +9,28 @@ import java.util.Scanner;
 public class CharToString {
 
     public static void main(String[] args){
-        char[] chars= {'h','e','l', 'l','o'};
-        String s= new String(chars);
-        System.out.print(s);
+//        char[] chars= {'h','e','l', 'l','o'};
+//        String s= new String(chars);
+//        System.out.print(s);
+//
+//        String s1= "This is a string";
+//        System.out.println(s1);
+//
+//
+//        String s2= new String("This is a string");
+//        System.out.println(s2);
+//
+//        Scanner scanner= new Scanner(System.in);
+//        System.out.print("Enter Value");
+//        String input= scanner.nextLine();
+//        System.out.print(" The result isgfhgfh " +input);
 
-        String s1= "This is a string";
-        System.out.println(s1);
+
+        System.out.println("Celsius 35 is Fahrenheit degree ");
+        System.out.println((9 / 5) * 35 + 32);
+        System.out.println("9 / 5 is "  + (9.0/5));
 
 
-        String s2= new String("This is a string");
-        System.out.println(s2);
-
-        Scanner scanner= new Scanner(System.in);
-        System.out.print("Enter Value");
-        String input= scanner.nextLine();
-        System.out.print(" The result is " +input);
     }
 }
 
