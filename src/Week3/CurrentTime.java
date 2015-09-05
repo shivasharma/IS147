9f01f12a9ff95b7ed2692f65c48db74bd818b077
Shiva Sharma <shivathebravo@gmail.com>
2015-09-05 10:51:33 -0400

    Added message

---
+++ src/Week3/CurrentTime.java
@@ -0,0 +1,34 @@
+package Week3;
+
+/**
+ * Created by Shiva on 9/5/2015.
+ */
+public class CurrentTime {
+    public static void main(String[] args) {
+
+        // Obtain the total milliseconds since the midnight, Jan 1, 1970
+        long totalMilliseconds = System.currentTimeMillis();
+
+        // Obtain the total seconds since the midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000;
+
+        // Compute the current second in the minute in the hour
+        long currentSecond = totalSeconds % 60;
+
+        // Obtain the total minutes
+        long totalMinutes = totalSeconds / 60;
+
+        // Compute the current minute in the hour
+        long currentMinute = totalMinutes % 60;
+
+        // Obtain the total hours
+        long totalHours = totalMinutes / 60;
+
+        // Compute the current hour
+        long currentHour = totalHours % 24;
+
+        // Display results
+        System.out.println("Current time is " + currentHour + ":"
+                + currentMinute + ":" + currentSecond + "GMT");
+    }
+}
