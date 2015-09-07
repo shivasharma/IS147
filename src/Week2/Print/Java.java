92c3ac7da7c128d5a7c040634852f3f7d6363c72
Shiva Sharma <shivathebravo@gmail.com>
2015-09-06 22:06:48 -0400

    Removed space

--- src/Week2/Print/Java.java
+++ src/Week2/Print/Java.java
@@ -4,7 +4,29 @@ package Week2.Print;
  * Created by Shiva on 8/30/2015.
  */
 public class Java {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         System.out.print("hello world 1");
+        int x = Integer.MAX_VALUE;
+        int y = Integer.MAX_VALUE + 1;
+
+        System.out.printf("%d %d%n", x, y);
+
+        int count=0;
+
+        System.out.println(count);
+
+        int number1 = 1;
+        int number2 = 2;
+        double average = (number1 + number2) / 2.0;
+        System.out.println(" This is a result of first avg" +average);
+
+        int number3 = 1;
+        int number4 = 2;
+        double average1 = (number3 + number4) / 2;
+        System.out.println(" This is a result of second avg" +average1);
+
+
     }
 }
