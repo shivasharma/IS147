fe340c2d1f9d62110673be4fe16d7d67d8a5811c
s.sharma <ss12@umbc.edu>
2015-09-10 14:32:08 -0400

    Added new file for parse function

---
+++ src/Week3/Parse.java
@@ -0,0 +1,17 @@
+package Week3;
+
+/**
+ * Created by s.sharma on 9/10/2015.
+ */
+public class Parse {
+
+    public static void main(String args[]){
+        int x =Integer.parseInt("10");
+        double c = Double.parseDouble("50");
+        int b = Integer.parseInt("712",16);
+
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
