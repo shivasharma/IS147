20c37d0e403b732c4d72a5cf408ed692fbcfd654
Shiva Sharma <shivathebravo@gmail.com>
2015-09-09 20:09:03 -0400

    Added new conversion file

---
+++ src/Week3/Conversion.java
@@ -0,0 +1,22 @@
+package Week3;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Shiva on 9/9/2015.
+ */
+public class Conversion {
+
+    public static void main(String[] args) {
+        int i=10;
+
+        float f=12.5f;
+        double d=23.5d;
+        int result=(int)f;
+        double floatConversion=(int)d;
+       // System.out.print (result);
+        System.out.println (floatConversion);
+        Integer ii=0;
+        System.out.println (ii.MAX_VALUE);
+    }
+}
