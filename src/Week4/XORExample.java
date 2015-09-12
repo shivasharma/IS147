773e13f0bdd34017ef992cf46b6bb168525aef21
Shiva Sharma <shivathebravo@gmail.com>
2015-09-12 13:45:44 -0400

    Exclusive XOR example

---
+++ src/Week4/XORExample.java
@@ -0,0 +1,31 @@
+package Week4;
+
+/**
+ * Created by Shiva on 9/12/2015.
+ */
+public class XORExample {
+
+
+        public static void main(String[]args){
+            boolean x = false;
+            boolean y = false;
+            boolean xXorY = x ^ y;
+            System.out.println("false XOR false: "+xXorY); //false
+
+            x = false;
+            y = true;
+            xXorY = x ^ y;
+            System.out.println("false XOR true: "+xXorY); //true
+
+            x = true;
+            y = false;
+            xXorY = x ^ y;
+            System.out.println("true XOR false: "+xXorY); //true
+
+            x = true;
+            y = true;
+            xXorY = x ^ y;
+            System.out.println("true XOR true: "+xXorY); //false
+    }
+
+}
