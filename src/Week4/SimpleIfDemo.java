8a35250f82d75ab726b5fb7cc4d4760497352f83
Shiva Sharma <shivathebravo@gmail.com>
2015-09-12 11:34:36 -0400

    Added new SimpleIfDemo file

---
+++ src/Week4/SimpleIfDemo.java
@@ -0,0 +1,22 @@
+package Week4;
+
+import java.util.Scanner;
+
+/**
+ * Created by Shiva on 9/12/2015.
+ */
+public class SimpleIfDemo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+        System.out.println ("Enter an integer: ");
+        int number = input.nextInt ();
+
+        if (number % 5 == 0)
+            System.out.println ("HiFive");
+
+        if (number % 2 == 0)
+            System.out.println ("HiEven");
+    }
+
+}
