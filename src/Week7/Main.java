d6190f630c1db040b51183855a916c199a1dba10
s.sharma <ss12@umbc.edu>
2015-10-05 21:12:14 -0400

    Methods in java

--- src/Week7/Main.java
+++ src/Week7/Main.java
@@ -9,17 +9,24 @@ public class Main {
 
     public static void main(String [] args){
         Person  person= new Person ();
-        person.name="shiva";
-        // int sum= m.Printnumber (2, 3);
-       // System.out.println (sum);
+        String personName= person.GetPersonName("shiva");
+
+        System.out.println(personName);
+
+
+        Method method=new Method();
+       int result= method.Printnumber(2,3);
+        System.out.println(result);
+
     }
 }
 
 
-/*
+
 class Method {
 
     public static int Printnumber(int x, int y){
         return x+y;
     }
-}*/
+}
+;
--- src/Week7/Person.java
+++ src/Week7/Person.java
@@ -4,8 +4,13 @@ package Week7;
  * Created by Shiva on 10/4/2015.
  */
 public class Person {
-     public String name ="Shiva";
-     public int age=0;
-    double height=0.0d;
+     private String name ="Shiva";
+    private int age=0;
+    private  double height=0.0d;
+
+
+    public String GetPersonName(String name){
+           return name;
+    }
 
 }
