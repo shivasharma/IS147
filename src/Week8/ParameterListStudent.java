86265fac2aea7779a454cea19ceb25efa37a5185
Shiva Sharma <shivathebravo@gmail.com>
2015-10-14 20:23:14 -0400

    CommandLine Parameter

---
+++ src/Week8/ParameterListStudent.java
@@ -0,0 +1,28 @@
+package Week8;
+
+/**
+ * Created by Shiva on 10/14/2015.
+ */
+public class ParameterListStudent {
+
+    public static void main(String[] args) {
+
+        int[] list = {2, 4, 6, 8, 9, 8};
+
+        double result = average (list);
+        System.out.println (result);
+    }
+
+
+    public static double average (int ... list)
+    {
+        double result = 0.0;
+        if (list.length != 0)
+        {    int sum = 0;
+            for (int num : list)
+                sum += num;
+            result = (double)sum / list.length;
+        }
+        return result;
+    }
+}
