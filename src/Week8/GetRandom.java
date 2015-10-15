632d7154248511f77374fd80bf2c5ae69daf1974
Shiva Sharma <shivathebravo@gmail.com>
2015-10-14 20:10:12 -0400

    ParameterList

---
+++ src/Week8/GetRandom.java
@@ -0,0 +1,27 @@
+package Week8;
+
+/**
+ * Created by Shiva on 10/14/2015.
+ */
+
+public class GetRandom {
+    public static void main(String[] args) {
+        System.out.println(getRandom(1, 2, 3, 10));
+    }
+
+    // Generate a random number between 1 and 54 excluding numbers
+    public static int getRandom(int ...numbers) {
+        generateRandom: while (true) {
+            int value = 1 + (int)(Math.random() * 54);
+
+            // Check if value is in numbers
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] == value) {
+                    continue generateRandom;
+                }
+            }
+
+            return value;
+        }
+    }
+}
