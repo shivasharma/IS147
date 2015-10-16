14c9910fa9a380c8fa88f15bf9cc028cd8c72755
Shiva Sharma <shivathebravo@gmail.com>
2015-10-15 23:25:02 -0400

    Week8

--- src/Week8/ParameterListStudent.java
+++ src/Week8/ParameterListStudent.java
@@ -7,10 +7,16 @@ public class ParameterListStudent {
 
     public static void main(String[] args) {
 
-        int[] list = {2, 4, 6, 8, 9, 8};
 
+        //First overload method called
+        int[] list = {2, 4, 6, 8, 9, 8};
         double result = average (list);
         System.out.println (result);
+
+        //Second overload method called
+        double[] listDouble = {2.5,3.5};
+        double resultDouble = average (listDouble);
+        System.out.println (resultDouble);
     }
 
 
@@ -22,6 +28,18 @@ public class ParameterListStudent {
             for (int num : list)
                 sum += num;
             result = (double)sum / list.length;
+        }
+        return result;
+    }
+
+    public static double average (double [] listDouble)
+    {
+        double result = 0.0;
+        if (listDouble.length != 0)
+        {   int sum = 0;
+            for (double num : listDouble)
+                sum += num;
+            result = (double)sum / listDouble.length;
         }
         return result;
     }
