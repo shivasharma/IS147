bb5d3492f7720fffffd040689be788dc7b64300f
Shiva Sharma <shivathebravo@gmail.com>
2015-11-01 11:50:58 -0500

    Enum

---
+++ src/Week10/Day.java
@@ -0,0 +1,9 @@
+package Week10;
+
+/**
+ * Created by Shiva on 11/1/2015.
+ */
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
--- src/Week10/JavaEnum.java
+++
@@ -1,7 +0,0 @@
-package Week10;
-
-/**
- * Created by Shiva on 11/1/2015.
- */
-public enum JavaEnum {
-}
