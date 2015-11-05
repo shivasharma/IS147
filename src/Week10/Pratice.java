a6772d7dbc2d644d1a03475b036899909cdc0a31
Shiva Sharma <shivathebravo@gmail.com>
2015-11-04 21:29:20 -0500

    In class pratice

---
+++ src/Week10/Pratice.java
@@ -0,0 +1,33 @@
+package Week10;
+
+/**
+ * Created by Shiva on 11/4/2015.
+ */
+
+public class Pratice
+{
+
+    // create 3 properties studentID, studentName, studentAge
+
+    //create default constructor and assign default values to above properties
+
+    //create parameterized constructor which takes 3 parameters
+    //pass and assign properties
+
+    // define Getter and setter methods for all properties
+
+
+}
+
+
+class TestOverloading
+{
+    public static void main(String args[])
+    {
+        //This object creation would call the default constructor
+        //StudentData()
+       /*This object creation would call the parameterized
+        * constructor StudentData(int, String, int)*/
+
+    }
+}
