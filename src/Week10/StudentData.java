2344983afac0a343dbf4ccab2934cc3e351fc08c
Shiva Sharma <shivathebravo@gmail.com>
2015-11-07 00:27:46 -0500

    Class and objects constructors

---
+++ src/Week10/StudentData.java
@@ -0,0 +1,34 @@
+package Week10;
+
+/**
+ * Created by Shiva on 11/4/2015.
+ */
+
+
+    public class StudentData
+    {
+
+        // create 3 private properties studentID, studentName, studentAge
+
+        //create default constructor and assign default values to above properties
+
+        //create parameterized constructor which takes 3 parameters
+        //pass and assign properties
+
+        // define Getter and setter methods for all properties
+
+
+    }
+
+
+class TestOverloading
+{
+    public static void main(String args[])
+    {
+        //This object creation would call the default constructor
+        //StudentData()
+       /*This object creation would call the parameterized
+        * constructor StudentData(int, String, int)*/
+
+    }
+}
