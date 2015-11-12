1d89f67b5f509b45e6a1d59a02ce0f7a4cc42f9d
Shiva Sharma <shivathebravo@gmail.com>
2015-11-11 22:20:01 -0500

    Composition: aggregation

---
+++ src/Week11/ClassLabAggregation/Circle.java
@@ -0,0 +1,37 @@
+package Week11.ClassLabAggregation;
+
+/**
+ * Created by Shiva on 11/11/2015.
+ */
+    //create Operation class
+    // define method name square with return type  int
+    // return n*n;
+        class Operation{
+            public void square(int n){
+                return n*n;
+            }
+        }
+
+
+class Circle{
+    //create operatoion class object here
+    // define Math.PI property
+        //call  square method inside the area method
+    // return the area of a square value;
+
+    double area(int radius){
+
+       return "How do I fix this";
+    }
+
+
+
+    public static void main(String args[]){
+
+        //create Circle class object
+        //call circle class area method pass some default value
+        //stroe the result of area method into new variable
+        //print the result using varialbe;
+
+    }
+}
--- src/Week11/Inheritance/Dog.java
+++ src/Week11/Inheritance/Dog.java
@@ -9,12 +9,12 @@ public class Dog extends Animal {
         System.out.println("A new dog has been created!");
     }
 
-    @Override
+
     public void sleep() {
         System.out.println("A dog sleeps...");
     }
 
-    @Override
+
     public void eat() {
         System.out.println("A dog eats...");
     }
