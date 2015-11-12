437041856a2f36f458b4a7e98e074743a5062d3b
Shiva Sharma <shivathebravo@gmail.com>
2015-11-11 21:17:57 -0500

    Calling Constructor using super

---
+++ src/Week11/ConstructorInheritance/Main.java
@@ -0,0 +1,12 @@
+package Week11.ConstructorInheritance;
+
+/**
+ * Created by Shiva on 11/11/2015.
+ */
+public class Main {
+
+    public static void main(String argd[]){
+        Subclass s= new Subclass(24);
+        s.getAge();
+    }
+}
---
+++ src/Week11/ConstructorInheritance/SuperClass.java
@@ -0,0 +1,16 @@
+package Week11.ConstructorInheritance;
+
+/**
+ * Created by Shiva on 11/11/2015.
+ */
+public class Superclass {
+    int age;
+
+    Superclass(int age){
+        this.age=age;
+    }
+
+    public void getAge(){
+        System.out.println("The value of the variable named age in super class is: " +age);
+    }
+}
