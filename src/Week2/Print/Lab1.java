be34329e29b8c3110258786665de3a9dd610749e
Shiva Sharma <shivathebravo@gmail.com>
2016-02-10 21:27:07 -0500

    lab1 Hello world

---
+++ src/Week2/Print/Lab1.java
@@ -0,0 +1,23 @@
+package Week2.Print;
+
+/**
+ * Created by Shiva on 2/10/2016.
+ */
+public class Lab1 {
+    /*
+    public static void main(String[] args)
+    {
+
+
+        System.out.print("This is my first message");
+
+        System.out.println ("This is my second message");
+
+        System.out.println("This is my third message");
+
+
+
+
+    }
+     */
+}
