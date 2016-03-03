fed00bfc3ce3bbe9aac140fd8396cd111045c483
Shiva Sharma <shivathebravo@gmail.com>
2016-03-02 21:13:46 -0500

    Loop examples

--- src/Week6/Loop.java
+++ src/Week6/Loop.java
@@ -1,44 +1,55 @@
-package com.company;
+
 import java.util.Scanner;
 public class Loop {
     //
-    public static void main(String[] args) {
-        // write your code here
-        Scanner in = new Scanner(System.in);
-        String H = "Hello World";
-        String h = "hello world";
 
-        //length
-        int l = h.length();
-        System.out.println("The length of the string is " + l);
-        //substring
-        String sub = H.substring(0,5);
-        System.out.println("A substring for the list is: " + sub);
-        //indexOf
-        int index = H.indexOf("d");
-        System.out.println(index);
-        //charAt
-        char ch = H.charAt(6);
-        System.out.println(ch);
-        //uppercase
-        String upperH = H.toUpperCase();
-        String lowerH = H.toLowerCase();
-        System.out.println(upperH);
-        System.out.println(lowerH);
-        //split
-        String[] split = H.split(" ");
-        String split1 = split[0];
-        String split2 = split[1];
-        System.out.println(split1 + " to the big " + split2);
-        //contains
-        boolean cont = H.contains("l");
-        boolean cont1 = H.contains("z");
-        System.out.println("The string H contains a l: " + cont + "\nSo does it contain a z: " + cont1);
-        //equalIgnoreCase
-        if (H.equalsIgnoreCase(h)) {
-            System.out.println("This is the coolest thing I have ever done.");
+        public static void main(String[] args) {
+
+            System.out.println ("---------- While Loop---------");
+            WhileLoop();
+
+            System.out.println ("----------Do While Loop---------");
+            DowhileLoop();
+
+            System.out.println ("----------For Loop---------");
+            ForLoop();
+
+
+
+        }
+
+
+    public static void WhileLoop(){
+        int i = 10;
+
+        while( i < 20 ) {
+            System.out.print("value of i : " + i );
+            i++;
+            System.out.print("\n");
+        }
+    }
+
+    public static void DowhileLoop(){
+        //Do while loop
+
+        int u = 10;
+
+        do{
+            System.out.print("value of do while : " + u );
+            u++;
+            System.out.print("\n");
+        }while( u < 20 );
+
+    }
+
+    public static void ForLoop(){
+        for(int n = 10; n < 20; n = n+1) {
+            System.out.print("value of n : " + n );
+            System.out.print("\n");
         }
     }
 
 }
+
+
 
