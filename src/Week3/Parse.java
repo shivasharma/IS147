05beded87f19d2262e2f259c08abc4c78877e6a9
shiva sharma <shivathebravo@gmail.com>
2016-09-13 20:40:08 -0400

    Loop examples

--- src/Week3/Parse.java
+++ src/Week3/Parse.java
@@ -8,10 +8,8 @@ public class Parse {
     public static void main(String args[]){
         int x =Integer.parseInt("10");
         double c = Double.parseDouble("50");
-        int b = Integer.parseInt("712",16);
-
-        System.out.println(x);
+               System.out.println(x);
         System.out.println(c);
-        System.out.println(b);
+
     }
 }
