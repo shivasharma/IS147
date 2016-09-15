182ce24456a4f75e2c27ddb11b1d11fa7c1985c8
shiva sharma <shivathebravo@gmail.com>
2016-09-14 22:49:03 -0400

    DataType parse Example

--- src/Week3/Parse.java
+++ src/Week3/Parse.java
@@ -11,5 +11,23 @@ public class Parse {
         System.out.println(x);
         System.out.println(c);
 
+
+        int intValue = 20;
+        System.out.println ("The value of intValue is:" + intValue);
+        long longValue = intValue;
+        System.out.println ("The value of longValue is:" + longValue);
+
+        short shortValue = (short) intValue;
+        System.out.println ("The value of loshortValues is:" + shortValue);
+
+
+        int intValue3 = 1024;
+        //lost data
+        byte byteValue = (byte) intValue3;
+        System.out.println ("The value of byte is:" + byteValue);
+
+        double doubleValue = 3.99999d;
+        int integerValue = (int) doubleValue;
+        System.out.println ("The value of integerValue is:" + integerValue);
     }
 }
