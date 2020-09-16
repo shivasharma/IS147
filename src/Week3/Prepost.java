5de396313951a9bd83c9ac63f5e096c75d1758ee
Shiva Sharma <ss12@umbc.edu>
2020-09-16 18:23:28 -0400

    Added code

--- src/Week3/Prepost.java
+++ src/Week3/Prepost.java
@@ -4,6 +4,18 @@ package Week3;
 public class Prepost {
 
     public static void main(String[] args) {
+
+        //prefix increment before
+        int someValue=5;
+        System.out.println(++someValue);
+        System.out.println(someValue);
+
+        //postfix increment after
+        int someOtherValue=5;
+        System.out.println(someOtherValue++);
+        System.out.println(someOtherValue);
+
+
         int num=0;
         int count=4;
         System.out.println ("If num is : "+num+" and count is: "+count);
