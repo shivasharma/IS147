ad66d70ec7bbc8d65785b2d79ed9ac49f9b9233b
Shiva Sharma <ss12@umbc.edu>
2020-09-16 17:28:41 -0400

    Updated code

---
+++ src/Week3/Prepost.java
@@ -0,0 +1,19 @@
+package Week3;
+
+
+public class Prepost {
+
+    public static void main(String[] args) {
+        int num=0;
+        int count=4;
+        System.out.println ("If num is : "+num+" and count is: "+count);
+        num=count++;
+        System.out.println ("Your postfix result of num=count++ is num= "
+                + num + " and count= " + count );
+
+        num=0;
+        count=4;
+        num=++count;
+        System.out.println ("Your prefix  result of num=++count is num= "+num + " and count= " + count);
+    }
+}
