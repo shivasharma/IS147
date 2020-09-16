aefafb7a031b1e15b75d2bef25bf74bcb7b5d71c
Shiva Sharma <ss12@umbc.edu>
2020-09-16 18:32:31 -0400

    OperatorPrecedence

---
+++ src/Week3/OperatorPrecedence.java
@@ -0,0 +1,19 @@
+
+package Week3;
+
+public class OperatorPrecedence {
+
+    //result 19,5
+    public static void main(String[] args) {
+     int valueA=21;
+     int valueB=6;
+     int valueC=3;
+     int valueD=1;
+     //  21-2
+     int result1=valueA-valueB/valueC;
+     //21-6=15/3
+     int result2=(valueA-valueB)/valueC;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
