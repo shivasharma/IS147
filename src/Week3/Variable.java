8c90b2e0a9d732782cb924f6d939c9e03d23eb00
Shiva Sharma <ss12@umbc.edu>
2020-09-16 18:14:09 -0400

    Added code variable

---
+++ src/Week3/Variable.java
@@ -0,0 +1,15 @@
+package Week3;
+
+
+public class Conversion {
+
+    public static void main(String[] args) {
+       int myVariable;
+       myVariable=20;
+        System.out.println(myVariable);
+        int anotherVariable=50;
+        System.out.println(anotherVariable);
+        myVariable=anotherVariable;
+        System.out.println(myVariable);
+    }
+}
