96fac56cafca7f51074665d314517c1d4c789c4d
Shiva Sharma <ss12@umbc.edu>
2020-09-30 16:04:11 -0400

    Update Immutable.java

--- src/Week5/Immutable.java
+++ src/Week5/Immutable.java
@@ -7,5 +7,8 @@ class Immutable{
         String s = "IS";
         s.concat(" 147");//concat() method appends the string at the end
         System.out.println(s);//will print IS because strings are immutable objects
+        
+        s=s.concat(" 147");
+        System.out.println(s);
     }
 }
