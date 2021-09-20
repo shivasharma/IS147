843df602892f394de7d92c11af1b0bcd679dde69
Shiva Sharma <ss12@umbc.edu>
2021-09-20 18:51:00 -0400

    Update Lab1.java

--- src/Week2/Print/Lab1.java
+++ src/Week2/Print/Lab1.java
@@ -7,7 +7,7 @@ public class Lab1 {
 
     public static void main(String[] args)
     {
-        system.out.println("This is my first message");
+        system.out.println("This is my first message";
 
         System.out.Println(This is my second message);
 
@@ -15,7 +15,7 @@ public class Lab1 {
 
         System.println("This is another message")
 
-          System.printLn("This is another message"
+          System.printLn("This is another message with erro"
 }
 
 }
