958a824ceb5d2c1d1e163ef19544a9919e6b1ced
Shiva Sharma <ss12@umbc.edu>
2021-09-26 10:53:49 -0400

    Updated code

--- src/Week3/classwork/classwork.java
+++ src/Week3/classwork/classwork.java
@@ -6,3 +6,36 @@ C:\Temp\out\production\Temp>java Main
 
 * if packages
         C:\Temp\out\production\Temp>java com.IS147.Main
+
+Variables area of storage
+
+        int myVariable=100;
+        int total;
+        into total100
+        int 200total
+
+        int sum;
+        //camelCase
+        int StudentTotal;
+        int bankAccountBalance
+
+
+
+Primitive data types
+
+  int myNumber;
+        myNumber=20;
+        System.out.println(myNumber);
+        int anotherNumber=30;
+        System.out.println(anotherNumber);
+
+        myNumber=antoherNumber;
+        System.out.println(anotherNumber);
+
+
+        Final
+      final int  myNumber=20;
+        final int someVarialbe;
+
+        int someOtherVariable=100;
+        someVariable=someOtherVariable;
