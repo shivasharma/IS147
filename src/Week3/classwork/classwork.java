fcfa05d66d4c54b0aa6668e9397cfe3130502e09
Shiva Sharma <ss12@umbc.edu>
2021-09-26 10:44:46 -0400

    Updated code

---
+++ src/Week3/classwork/classwork.java
@@ -0,0 +1,8 @@
+/* Build the project  and run via command line
+for e.g.class  C:\Temp\Javaproject\out\porduction\MyApp
+java Main
+*/
+C:\Temp\out\production\Temp>java Main
+
+* if packages
+        C:\Temp\out\production\Temp>java com.IS147.Main
