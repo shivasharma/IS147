699449970397ad1b60c08c71f437a585eaade01e
Shiva Sharma <ss12@umbc.edu>
2021-10-04 13:08:04 -0400

    Updated code

---
+++ src/Week4/Concatenate.java
@@ -0,0 +1,10 @@
+public class ChineseZodiac {
+    public static void main(String[] args) {
+        String str1 = "I am ";
+
+        String str2 = null;
+
+        String str3 = str1 + str2;
+
+    }
+}
