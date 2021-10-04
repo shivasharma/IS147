a8e7504447c2245e89032061a31539945513fb66
Shiva Sharma <ss12@umbc.edu>
2021-10-04 18:42:14 -0400

    Update SimpleIfDemo.java

--- src/Week4/SimpleIfDemo.java
+++ src/Week4/SimpleIfDemo.java
@@ -3,6 +3,22 @@ package Week4;
 import java.util.Scanner;
 public class SimpleIfDemo {
     public static void main(String[] args) {
+        
+        
+        //part1
+        int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+
+#Short Hand If...Else (Ternary Operator)
+
+int time = 20;
+String result = (time < 18) ? "Good day." : "Good evening.";
+System.out.println(result);
+        
         Scanner input = new Scanner (System.in);
         System.out.print ("Enter an integer: ");
         int number = input.nextInt ();
