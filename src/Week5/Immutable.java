3168f6d8a52523a901562fcb3bea689e181edf7c
Shiva Sharma <ss12@umbc.edu>
2021-10-11 09:47:11 -0400

    Update Immutable.java

--- src/Week5/Immutable.java
+++ src/Week5/Immutable.java
@@ -4,6 +4,32 @@ package Week5;
 
 class Immutable{
     public static void main(String[] args) {
+        
+        String s1="Hello";
+        String s2= "Hello";
+        //Assigning a literal value  asking java complier to find the best way to creat a string. String are object reference type not primitive 
+        //This process is also called interning
+        if(s1==s2){
+             System.out.println("They are match);
+        }else{
+            System.out.println("The do not match");
+        }
+            
+        String s3= new String("Hello");
+        String s4= new String("Hello");
+                                
+         if(s3==s4){
+             System.out.println("They are match);
+        }else{
+            System.out.println("The do not match");
+        }                    
+                                
+            if(s3.equals(s4)){
+             System.out.println("They are match);
+        }else{
+            System.out.println("The do not match");
+        }                           
+        
         String s = "IS";
         s.concat(" 147");//concat() method appends the string at the end
         System.out.println(s);//will print IS because strings are immutable objects
