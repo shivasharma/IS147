2876e352c4808eba57f5004262966995fa10fbcf
Shiva Sharma <ss12@umbc.edu>
2021-10-25 12:07:58 -0400

    Create AmbiguousOverloading.java

---
+++ src/Week7/AmbiguousOverloading.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+        System.out.println(sum(12,20));
+        System.out.println(sum(30,40));
+
+    }
+
+        public  static long sum(int i, long j) {
+            return i+j;
+        }
+        public static long sum(long i, int j) {
+            return i+j;
+        }
+}
