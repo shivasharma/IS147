2a5cbe31c0b7cd752a406d37ccff675e3f53ab59
Shiva Sharma <ss12@umbc.edu>
2021-10-25 11:02:56 -0400

    Updated code

---
+++ src/Week7/Classwork/Classwork.java
@@ -0,0 +1,21 @@
+
+1) Insert the missing part to call myMethod from main.
+static void myMethod() {
+        System.out.println("I just got executed!");
+        }
+
+2)Insert the missing part to call myMethod from main two times.
+static void myMethod(){
+        System.out.println("I just got executed!");
+        }
+
+3) Add a fname parameter of type String to myMethod, and output "John Doe".
+
+static void myMethod(...     ....)
+        System.out.println(...+ " Doe");
+
+
+4) Insert the missing part to print the number 8 in main, by using a specific keyword inside myMethod:
+       static int myMethod(int x) {
+        ...... 5 + x;
+        }
