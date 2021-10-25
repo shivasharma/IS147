36898814377c4f03ad410bac695a942557a057d3
Shiva Sharma <ss12@umbc.edu>
2021-10-25 11:18:15 -0400

    Updated code

--- src/Week7/ReturnValueFromMethod.java
+++ src/Week7/ReturnValueFromMethod.java
@@ -6,12 +6,26 @@ public class ReturnValueFromMethod {
     public static void main(String[] args)
     {
 
+        int z=max(10,20);
+        System.out.println(z);
+
        int number=25;
         System.out.println( number + " is " + sub(number));
 
         if(sub2(number)){
             System.out.println( number + " is above 10");
         }
+    }
+
+
+    public static int max (int num1,int num2){
+        int result;
+        if(num1>num2){
+            result=num1;
+        }else{
+            return =num2;
+        }
+        return result;
     }
 
     private static String sub(int num) {
