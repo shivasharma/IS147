0f632ac3da7a20b3f71323104108142aea100971
Shiva Sharma <ss12@umbc.edu>
2021-11-15 18:32:08 -0500

    Create gettersetter.java

---
+++ src/Week10/gettersetter.java
@@ -0,0 +1,23 @@
+public class Person {
+  private String name; // private = restricted access
+
+  // Getter
+  public String getName() {
+    return name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.name = newName;
+  }
+}
+
+public static void main(String[] args) {
+    Person myObj = new Person();
+    myObj.name = "John";  // error
+    System.out.println(myObj.name); // error 
+  }
+
+Person myObj = new Person();
+    myObj.setName("John"); // Set the value of the name variable to "John"
+    System.out.println(myObj.getName());
