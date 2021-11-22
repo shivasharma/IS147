df971ae1d13ae4a4573f7894de0fcaadd9f1366f
Shiva Sharma <ss12@umbc.edu>
2021-11-22 09:11:49 -0500

    Updated code

---
+++ src/Week11/WhatIsThis/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Dog myDog=new Dog();
+        myDog.setName("ABC");
+        System.out.println(myDog.getName());
+
+
+    }
+}
