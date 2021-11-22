124b80ff10333128105e63935687fa4a1bc160b6
Shiva Sharma <ss12@umbc.edu>
2021-11-22 09:07:29 -0500

    Updated code

--- src/Week11/WhatIsThis/Dog.java
+++ src/Week11/WhatIsThis/Dog.java
@@ -1,12 +1,14 @@
 public class Dog{
     private String name;
     private int age;
-//this refer to the dog object here
+//this refer to the dog object here//this is
     public Dog (String name, int age){
         this.name=name;
         this.age=age;
     }
-
+  public static void doSomeThing(){
+        this.name;
+    }
     public void setName(string name){
         //this set the local name value
         name=name;
