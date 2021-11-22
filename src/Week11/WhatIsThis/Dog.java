d98ab30f9cb5a5e1c3a68e1228524133caff1430
Shiva Sharma <ss12@umbc.edu>
2021-11-22 09:15:16 -0500

    Updated code

--- src/Week11/WhatIsThis/Dog.java
+++ src/Week11/WhatIsThis/Dog.java
@@ -1,14 +1,22 @@
 public class Dog{
     private String name;
     private int age;
-//this refer to the dog object here//this is
+    public Dog(){
+        this("ABC",1);
+    }
+//this refer to the dog object here
+// this  operator cannot be used on static method
     public Dog (String name, int age){
         this.name=name;
         this.age=age;
     }
+    //this is invalid
+ /*
   public static void doSomeThing(){
         this.name;
     }
+    */
+
     public void setName(string name){
         //this set the local name value
         name=name;
