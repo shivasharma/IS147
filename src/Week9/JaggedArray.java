9979879cf0f8db6f3e210a1c7a40f9dac3cae713
Shiva Sharma <ss12@umbc.edu>
2022-04-06 18:58:49 -0400

    Update JaggedArray.java

--- src/Week9/JaggedArray.java
+++ src/Week9/JaggedArray.java
@@ -1,17 +1,20 @@
-	   // Declare a 2-D array with 3 rows
-       int myarray[][] = new int[3][];
- 
-       // define and initialize jagged array
- 
-       myarray[0] = new int[]{1,2,3};
-       myarray[1] = new int[]{4,5};
-       myarray[2] = new int[]{6,7,8,9,10};
- 
-       // display the jagged array
-       System.out.println("Two dimensional Jagged Array:");
-       for (int i=0; i&lt;myarray.length; i++)
-       {
-          for (int j=0; j&lt;myarray[i].length; j++)
-              System.out.print(myarray[i][j] + " ");
-          System.out.println();
+public class Main {
+
+    public static void main(String[] args) {
+        // Declare a 2-D array with 3 rows
+        int myarray[][] = new int[3][];
+        // define and initialize jagged array
+        myarray[0] = new int[]{1,2,3};
+        myarray[1] = new int[]{4,5};
+        myarray[2] = new int[]{6,7,8,9,10};
+
+        // display the jagged array
+        System.out.println("Two dimensional Jagged Array:");
+        for (int i=0; i<myarray.length; i++)
+        {
+            for (int j=0; j<myarray[i].length; j++)
+            System.out.print(myarray[i][j] + " ");
+            System.out.println();
         }
+    }
+}
