b5b84750bc711dd48902a87c24115dd05c9057e3
Shiva <ss12@umbc.edu>
2022-05-02 09:38:20 -0400

    Updated code

---
+++ src/Javadocs/Main.java
@@ -0,0 +1,28 @@
+/**
+ * This a main class to test Person.java
+ *
+ * @author Shiva Sharma
+ * @version 1.0
+ * @see Person
+ */
+public class Main {
+
+    //https://www.oracle.com/technical-resources/articles/java/javadoc-tool.html
+    //part 1 : description
+    //part 2 : tage list
+    //How to run javadoc
+    //1. cmd line: javadoc -d doc src\* or  javadoc -d doc src\* -author -version
+
+    //2 Intellij IDEA IDE
+
+    /**
+     * This is a main method
+     *
+     * @param args stores incoming the command line arg for the program
+     */
+    public static void main(String[] args) {
+        Person person = new Person("Shiva Sharma");
+
+
+    }
+}
---
+++ src/Javadocs/Student.java
@@ -0,0 +1,13 @@
+/**
+ * Example of Inheritance class
+ */
+public class Student extends Person {
+    /**
+     * This is a construction to initialize person object
+     *
+     * @param name inital person name
+     */
+    public Student(String name) {
+        super(name);
+    }
+}
