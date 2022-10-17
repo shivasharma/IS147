7669979b26c2b3ead4a54a734d6be512519f6f31
Shiva Sharma <ss12@umbc.edu>
2022-10-17 19:01:01 -0400

    Update MultiMethod.java

--- src/Week7/MultiMethod.java
+++ src/Week7/MultiMethod.java
@@ -9,11 +9,23 @@ public class MultiMethod {
         System.out.println("Before calling  method");
         doSomething();
         System.out.println("After calling  method");
+        
+         System.out.println("The max value is " + max(5,2));
     }
 
     //void method
     public static void doSomething(){
         System.out.println("Message from doSomething method");
     }
+    
+     public static int max(int num1,num2){
+    int result;
+	if(num1>num2){
+	  result=num1;
+	}else{
+	result=num2
+	}
+	return result;
+   }
 
 }
