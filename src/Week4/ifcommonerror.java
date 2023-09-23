d4c3bc34b24312ab435b149cd832118932c0c488
Shiva Sharma <ss12@umbc.edu>
2023-09-23 10:10:58 -0400

    Update ifcommonerror.java

--- src/Week4/ifcommonerror.java
+++ src/Week4/ifcommonerror.java
@@ -1,1 +1,11 @@
-
+ public static void main(String args[]) {
+   // adding; at the if statement is wrong
+								if (radius >= 0);   
+								{
+								  area = radius*radius*PI;
+								  System.out.println(
+									"The area for the circle of radius " +
+									radius + " is " + area);
+								}
+								
+  }
