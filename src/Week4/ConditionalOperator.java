d57d2a50038be15414d9ac2d1e8d11caed54a4f7
Shiva Sharma <ss12@umbc.edu>
2023-09-24 08:50:29 -0400

    Update ConditionalOperator.java

--- src/Week4/ConditionalOperator.java
+++ src/Week4/ConditionalOperator.java
@@ -1,6 +1,9 @@
 public class ConditionalOperator {
-    public static void main(String[] args) {
-
+   public static void main(String[] args) {
+        conditional_operator1();
+        conditional_operator2();
+    }
+    public static void conditional_operator1(){
         int x = 0;
         int y = 0;
         if (x > 0) {
@@ -14,4 +17,14 @@ public class ConditionalOperator {
         y = (x > 0) ? 1 : -1;
         System.out.println("Printing second y" +y);
     }
+    public static void conditional_operator2(){
+        int num=5;
+        if (num % 2 == 0)
+            System.out.println(num + "is even");
+        else
+            System.out.println(num + " is odd");
+
+    }
 }
+
+
