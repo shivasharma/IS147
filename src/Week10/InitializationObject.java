d6fbbba47005bf5cc29e52e5bdefcd915971fdec
Shiva Sharma <ss12@umbc.edu>
2024-04-01 13:11:33 -0400

    Update InitializationObject.java

--- src/Week10/InitializationObject.java
+++ src/Week10/InitializationObject.java
@@ -1,1 +1,19 @@
+class MyClass{
+    public int number;
+    public String name;
 
+  public MyClass(){
+    number=20;
+    name="Shiva";
+  }
+}
+
+public class InitializationObject
+{
+   public static void main (String[] args){
+
+     MyClass myClass=new MyClass();
+     System.out.println(myClass.number);
+     System.out.println(myClass.name);
+   }
+}
