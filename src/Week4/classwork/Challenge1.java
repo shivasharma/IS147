3c523b2f5b5e8b9160ce4853fede7d5c2b52c3fe
Shiva Sharma <ss12@umbc.edu>
2024-09-23 16:12:04 -0400

    Update Challenge1.java

--- src/Week4/classwork/Challenge1.java
+++ src/Week4/classwork/Challenge1.java
@@ -4,7 +4,7 @@ public class Challenge1 {
 
         // How do you print 2?
         // How do you print 4?
-       // Write another else if condfition to check k less than or equal  conditin and print 5
+       // Write another else if condition to check k less than or equal  conditin and print 5
         
         int k = 5;
         if(k > 20) {
