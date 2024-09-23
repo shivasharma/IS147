04cd54ae00916deae5b965ec76571d9faf13f50c
Shiva Sharma <ss12@umbc.edu>
2024-09-23 15:06:32 +0000

    added challenge code

---
+++ src/Week4/classwork/Challenge1.java
@@ -0,0 +1,18 @@
+public class Challenge1 {
+    public static void main(String[] args)
+    {
+
+        // How do you print 2?
+        int k = 20;
+        if(k > 20) {
+            System.out.println(1);
+        } else if(k > 10) {
+            System.out.println(2);
+        } else if(k < 5) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+
+}
