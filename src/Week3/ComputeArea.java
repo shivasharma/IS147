56a21cb479f322cfc0242ec84dd95f818e23f263
Shiva Sharma <ss12@umbc.edu>
2025-02-09 08:31:49 -0500

    Create ComputeArea.java

---
+++ src/Week3/ComputeArea.java
@@ -0,0 +1,14 @@
+public class ComputeArea {
+    /** Main method */
+    public static void main(String[] args) {
+      double radius;
+      double area;
+      // Assign a radius
+      radius = 20;
+      // Compute area
+      area = radius * radius * 3.14159;
+      // Display results
+      System.out.println("The area for the circle of radius " +
+        radius + " is " + area);
+    }
+  }
