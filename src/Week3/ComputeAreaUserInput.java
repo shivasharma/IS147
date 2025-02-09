dbc31d8fa635084ec9792cd7d11cdb4b7b47d121
Shiva Sharma <ss12@umbc.edu>
2025-02-09 09:30:37 -0500

    Update ComputeAreaUserInput.java

--- src/Week3/ComputeAreaUserInput.java
+++ src/Week3/ComputeAreaUserInput.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-              // Create a Scanner object
-            Scanner input = new Scanner(System.in);
-            // Prompt the user to enter a radius
-            System.out.print("Enter a number for radius: ");
-            double radius = input.nextDouble();
-            // Compute area
-            double area = radius * radius * 3.14159;
-            // Display result
-            System.out.println("The area for the circle of radius " +
-                    radius + " is " + area);
-        }
+public static void main(String[] args) {
+    // Create a Scanner object
+     Scanner input = new Scanner(System.in);
+    // Prompt the user to enter a radius
+    System.out.print("Enter a number for radius: ");
+     double radius = input.nextDouble();
+     // Compute area
+     double area = radius * radius * 3.14159;
+     // Display result
+    System.out.println("The area for the circle of radius " +
+         radius + " is " + area);
+     }
 }
